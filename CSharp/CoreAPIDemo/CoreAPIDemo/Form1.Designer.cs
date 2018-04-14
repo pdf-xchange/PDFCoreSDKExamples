8cf8f6de4e16992462bfbe72f9ccf721aef4965b
palamar <polaringu@tracker-software.com>
2018-04-14 10:47:07 +0300

    Created a C# project

---
+++ CSharp/CoreAPIDemo/CoreAPIDemo/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace CoreAPIDemo
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Create New Document");
+			System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Open Document");
+			System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Document", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2});
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.sampleTree = new System.Windows.Forms.TreeView();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.splitter1 = new System.Windows.Forms.Splitter();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.runSample = new System.Windows.Forms.ToolStripButton();
+			this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+			this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+			this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.toolStrip1.SuspendLayout();
+			this.toolStrip2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// sampleTree
+			// 
+			this.sampleTree.Dock = System.Windows.Forms.DockStyle.Left;
+			this.sampleTree.Location = new System.Drawing.Point(0, 32);
+			this.sampleTree.Name = "sampleTree";
+			treeNode1.Name = "createNewDoc";
+			treeNode1.Text = "Create New Document";
+			treeNode2.Name = "openDoc";
+			treeNode2.Text = "Open Document";
+			treeNode3.Name = "Document";
+			treeNode3.Text = "Document";
+			this.sampleTree.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode3});
+			this.sampleTree.Size = new System.Drawing.Size(802, 892);
+			this.sampleTree.TabIndex = 0;
+			this.sampleTree.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.sampleTree_NodeMouseClick);
+			// 
+			// panel1
+			// 
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1552, 924);
+			this.panel1.TabIndex = 1;
+			// 
+			// splitter1
+			// 
+			this.splitter1.Location = new System.Drawing.Point(802, 32);
+			this.splitter1.Name = "splitter1";
+			this.splitter1.Size = new System.Drawing.Size(10, 892);
+			this.splitter1.TabIndex = 0;
+			this.splitter1.TabStop = false;
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runSample});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(1552, 32);
+			this.toolStrip1.TabIndex = 0;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// runSample
+			// 
+			this.runSample.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.runSample.Image = ((System.Drawing.Image)(resources.GetObject("runSample.Image")));
+			this.runSample.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.runSample.Name = "runSample";
+			this.runSample.Size = new System.Drawing.Size(111, 29);
+			this.runSample.Text = "Run Sample";
+			// 
+			// toolStrip2
+			// 
+			this.toolStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
+			this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripTextBox1,
+            this.toolStripButton2});
+			this.toolStrip2.Location = new System.Drawing.Point(812, 32);
+			this.toolStrip2.Name = "toolStrip2";
+			this.toolStrip2.Size = new System.Drawing.Size(740, 31);
+			this.toolStrip2.TabIndex = 0;
+			this.toolStrip2.Text = "toolStrip2";
+			// 
+			// toolStripButton1
+			// 
+			this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+			this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton1.Name = "toolStripButton1";
+			this.toolStripButton1.Size = new System.Drawing.Size(28, 28);
+			this.toolStripButton1.Text = "toolStripButton1";
+			// 
+			// toolStripTextBox1
+			// 
+			this.toolStripTextBox1.Name = "toolStripTextBox1";
+			this.toolStripTextBox1.Size = new System.Drawing.Size(100, 31);
+			// 
+			// toolStripButton2
+			// 
+			this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+			this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton2.Name = "toolStripButton2";
+			this.toolStripButton2.Size = new System.Drawing.Size(28, 28);
+			this.toolStripButton2.Text = "toolStripButton2";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Location = new System.Drawing.Point(812, 63);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(740, 861);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1552, 924);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.toolStrip2);
+			this.Controls.Add(this.splitter1);
+			this.Controls.Add(this.sampleTree);
+			this.Controls.Add(this.toolStrip1);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.toolStrip2.ResumeLayout(false);
+			this.toolStrip2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TreeView sampleTree;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Splitter splitter1;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripButton runSample;
+		private System.Windows.Forms.ToolStrip toolStrip2;
+		private System.Windows.Forms.ToolStripButton toolStripButton1;
+		private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+		private System.Windows.Forms.ToolStripButton toolStripButton2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
---
+++ CSharp/CoreAPIDemo/CoreAPIDemo/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoreAPIDemo
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void sampleTree_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+		{
+			Type thisType = this.GetType();
+			MethodInfo theMethod = thisType.GetMethod(e.Node.Name);
+			if (theMethod != null)
+				theMethod.Invoke(this, null);
+		}
+
+		public void createNewDoc()
+		{
+			int a = 0;
+		}
+	}
+}
