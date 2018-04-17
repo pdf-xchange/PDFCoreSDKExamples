7a0457ce68b9c2866b3b86fdfc10c2a4d4b511b4
Dima <dima@tracker-software.com>
2018-04-17 15:15:51 +0300

    Add left panel

--- CSharp/CoreAPIDemo/CoreAPIDemo/Form1.Designer.cs
+++ CSharp/CoreAPIDemo/CoreAPIDemo/Form1.Designer.cs
@@ -43,26 +43,36 @@
 			this.splitter2 = new System.Windows.Forms.Splitter();
 			this.codeSource = new FastColoredTextBoxNS.FastColoredTextBox();
 			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.toolStrip3 = new System.Windows.Forms.ToolStrip();
+			this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
 			this.toolStrip1.SuspendLayout();
 			this.toolStrip2.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.previewImage)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.codeSource)).BeginInit();
 			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.toolStrip3.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// sampleTree
 			// 
-			this.sampleTree.Dock = System.Windows.Forms.DockStyle.Left;
-			this.sampleTree.Location = new System.Drawing.Point(0, 0);
+			this.sampleTree.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.sampleTree.FullRowSelect = true;
+			this.sampleTree.HotTracking = true;
+			this.sampleTree.Location = new System.Drawing.Point(0, 35);
 			this.sampleTree.Name = "sampleTree";
-			this.sampleTree.Size = new System.Drawing.Size(686, 899);
+			this.sampleTree.Size = new System.Drawing.Size(572, 864);
 			this.sampleTree.TabIndex = 0;
 			this.sampleTree.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.sampleTree_NodeMouseClick);
 			this.sampleTree.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.sampleTree_NodeMouseDoubleClick);
 			// 
 			// splitter1
 			// 
-			this.splitter1.Location = new System.Drawing.Point(686, 0);
+			this.splitter1.Location = new System.Drawing.Point(572, 0);
 			this.splitter1.Name = "splitter1";
 			this.splitter1.Size = new System.Drawing.Size(10, 899);
 			this.splitter1.TabIndex = 0;
@@ -76,7 +86,7 @@
             this.runSample});
 			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
 			this.toolStrip1.Name = "toolStrip1";
-			this.toolStrip1.Size = new System.Drawing.Size(841, 35);
+			this.toolStrip1.Size = new System.Drawing.Size(987, 35);
 			this.toolStrip1.TabIndex = 0;
 			this.toolStrip1.Text = "toolStrip1";
 			// 
@@ -98,9 +108,9 @@
             this.currentPage,
             this.pagesCount,
             this.nextPage});
-			this.toolStrip2.Location = new System.Drawing.Point(696, 0);
+			this.toolStrip2.Location = new System.Drawing.Point(0, 0);
 			this.toolStrip2.Name = "toolStrip2";
-			this.toolStrip2.Size = new System.Drawing.Size(841, 31);
+			this.toolStrip2.Size = new System.Drawing.Size(987, 35);
 			this.toolStrip2.TabIndex = 0;
 			this.toolStrip2.Text = "toolStrip2";
 			// 
@@ -116,6 +126,8 @@
 			// 
 			// currentPage
 			// 
+			this.currentPage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.currentPage.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
 			this.currentPage.Name = "currentPage";
 			this.currentPage.Size = new System.Drawing.Size(50, 31);
 			this.currentPage.Text = "1";
@@ -140,19 +152,20 @@
 			// previewImage
 			// 
 			this.previewImage.BackColor = System.Drawing.SystemColors.Control;
+			this.previewImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.previewImage.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.previewImage.Location = new System.Drawing.Point(696, 0);
+			this.previewImage.Location = new System.Drawing.Point(0, 35);
 			this.previewImage.Name = "previewImage";
-			this.previewImage.Size = new System.Drawing.Size(841, 472);
+			this.previewImage.Size = new System.Drawing.Size(987, 483);
 			this.previewImage.TabIndex = 0;
 			this.previewImage.TabStop = false;
 			// 
 			// splitter2
 			// 
 			this.splitter2.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.splitter2.Location = new System.Drawing.Point(696, 518);
+			this.splitter2.Location = new System.Drawing.Point(582, 518);
 			this.splitter2.Name = "splitter2";
-			this.splitter2.Size = new System.Drawing.Size(841, 10);
+			this.splitter2.Size = new System.Drawing.Size(987, 10);
 			this.splitter2.TabIndex = 0;
 			this.splitter2.TabStop = false;
 			this.splitter2.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitter2_SplitterMoved);
@@ -180,6 +193,7 @@
 			this.codeSource.Cursor = System.Windows.Forms.Cursors.IBeam;
 			this.codeSource.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
 			this.codeSource.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.codeSource.Font = new System.Drawing.Font("Courier New", 9.75F);
 			this.codeSource.IsReplaceMode = false;
 			this.codeSource.Language = FastColoredTextBoxNS.Language.CSharp;
 			this.codeSource.LeftBracket = '(';
@@ -192,7 +206,7 @@
 			this.codeSource.RightBracket2 = '}';
 			this.codeSource.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
 			this.codeSource.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("codeSource.ServiceColors")));
-			this.codeSource.Size = new System.Drawing.Size(873, 336);
+			this.codeSource.Size = new System.Drawing.Size(987, 336);
 			this.codeSource.TabIndex = 2;
 			this.codeSource.Zoom = 100;
 			// 
@@ -201,22 +215,69 @@
 			this.panel1.Controls.Add(this.codeSource);
 			this.panel1.Controls.Add(this.toolStrip1);
 			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.panel1.Location = new System.Drawing.Point(696, 528);
+			this.panel1.Location = new System.Drawing.Point(582, 528);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(873, 371);
+			this.panel1.Size = new System.Drawing.Size(987, 371);
 			this.panel1.TabIndex = 3;
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.previewImage);
+			this.panel2.Controls.Add(this.toolStrip2);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel2.Location = new System.Drawing.Point(582, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(987, 518);
+			this.panel2.TabIndex = 4;
+			// 
+			// panel3
+			// 
+			this.panel3.Controls.Add(this.sampleTree);
+			this.panel3.Controls.Add(this.toolStrip3);
+			this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel3.Location = new System.Drawing.Point(0, 0);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(572, 899);
+			this.panel3.TabIndex = 5;
+			// 
+			// toolStrip3
+			// 
+			this.toolStrip3.ImageScalingSize = new System.Drawing.Size(24, 24);
+			this.toolStrip3.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1,
+            this.toolStripButton1});
+			this.toolStrip3.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip3.Name = "toolStrip3";
+			this.toolStrip3.Size = new System.Drawing.Size(572, 35);
+			this.toolStrip3.TabIndex = 1;
+			this.toolStrip3.Text = "toolStrip3";
+			// 
+			// toolStripTextBox1
+			// 
+			this.toolStripTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.toolStripTextBox1.Margin = new System.Windows.Forms.Padding(1, 2, 1, 2);
+			this.toolStripTextBox1.Name = "toolStripTextBox1";
+			this.toolStripTextBox1.Size = new System.Drawing.Size(500, 31);
+			// 
+			// toolStripButton1
+			// 
+			this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+			this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton1.Name = "toolStripButton1";
+			this.toolStripButton1.Size = new System.Drawing.Size(28, 31);
+			this.toolStripButton1.Text = "toolStripButton1";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1569, 899);
-			this.Controls.Add(this.toolStrip2);
+			this.Controls.Add(this.panel2);
 			this.Controls.Add(this.splitter2);
-			this.Controls.Add(this.previewImage);
 			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.splitter1);
-			this.Controls.Add(this.sampleTree);
+			this.Controls.Add(this.panel3);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
@@ -231,8 +292,13 @@
 			((System.ComponentModel.ISupportInitialize)(this.codeSource)).EndInit();
 			this.panel1.ResumeLayout(false);
 			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			this.toolStrip3.ResumeLayout(false);
+			this.toolStrip3.PerformLayout();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
@@ -251,6 +317,11 @@
 		private System.Windows.Forms.Splitter splitter2;
 		private FastColoredTextBoxNS.FastColoredTextBox codeSource;
 		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.ToolStrip toolStrip3;
+		private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+		private System.Windows.Forms.ToolStripButton toolStripButton1;
 	}
 }
 
