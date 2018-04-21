e6afe80947f92aaea80eb5fe8abc036d40908d39
palamar <polaringu@tracker-software.com>
2018-04-21 16:21:28 +0300

    Added Form Icon

--- CSharp/CoreAPIDemo/CoreAPIDemo/Form1.Designer.cs
+++ CSharp/CoreAPIDemo/CoreAPIDemo/Form1.Designer.cs
@@ -332,6 +332,7 @@
 			this.Controls.Add(this.panel1);
 			this.Controls.Add(this.splitter1);
 			this.Controls.Add(this.panel3);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.MinimumSize = new System.Drawing.Size(1280, 960);
 			this.Name = "Form1";
 			this.Text = "CoreAPIDemo";
