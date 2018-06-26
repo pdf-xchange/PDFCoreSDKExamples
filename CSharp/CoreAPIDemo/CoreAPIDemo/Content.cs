0c8e10c6271c72be44b1f9f3f4491f7adcfec8cc
palamar <polaringu@tracker-software.com>
2018-06-26 09:40:12 +0300

    Added content declarations

---
+++ CSharp/CoreAPIDemo/CoreAPIDemo/Content.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using PDFXCoreAPI;
+
+namespace CoreAPIDemo
+{
+	[Description("Content")]
+	public class Content
+	{
+		[Description("Add Text with different Fonts and Styles to the current document")]
+		static public void DrawTextOnPage(Form1 Parent)
+		{
+			if (Parent.m_CurDoc == null)
+				Document.CreateNewDoc(Parent);
+		}
+
+		[Description("Add Arcs with different Styles to the current document")]
+		static public void DrawArcsOnPage(Form1 Parent)
+		{
+			if (Parent.m_CurDoc == null)
+				Document.CreateNewDoc(Parent);
+		}
+
+		[Description("Add Polygons and Curves with different Styles to the current document")]
+		static public void DrawPolygonsAndCurvesOnPage(Form1 Parent)
+		{
+			if (Parent.m_CurDoc == null)
+				Document.CreateNewDoc(Parent);
+		}
+
+		[Description("Add Fillings with different Styles to the current document")]
+		static public void DrawFillingsOnPage(Form1 Parent)
+		{
+			if (Parent.m_CurDoc == null)
+				Document.CreateNewDoc(Parent);
+		}
+
+		[Description("Add Patterns with different Styles to the current document")]
+		static public void DrawPatternsOnPage(Form1 Parent)
+		{
+			if (Parent.m_CurDoc == null)
+				Document.CreateNewDoc(Parent);
+		}
+
+		[Description("Add content with different Coordinate System Transformations (matrix usages)")]
+		static public void AddCoordinateSystemTransformations(Form1 Parent)
+		{
+			if (Parent.m_CurDoc == null)
+				Document.CreateNewDoc(Parent);
+		}
+	}
+}
