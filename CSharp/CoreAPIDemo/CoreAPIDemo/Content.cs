9c66d411a7d7452c8a29d1f058ebfa775edbfa36
Arsen Senkiv <senkivarsen@gmail.com>
2018-06-28 13:08:40 +0300

    Desribe Samples: "Add Patterns with different Styles to the current document", "Add Text with different Text Rendering Mode to the current document"

--- CSharp/CoreAPIDemo/CoreAPIDemo/Content.cs
+++ CSharp/CoreAPIDemo/CoreAPIDemo/Content.cs
@@ -15,21 +15,225 @@ namespace CoreAPIDemo
 		delegate void DrawArrLine(IPXC_ContentCreator CC, double xfrom, double yfrom, double xto, double yto, double linewidth, bool bDashed);
 		delegate void DrawCS(IPXC_ContentCreator CC, double x0, double y0, double w, double h);
 		delegate void DrawN(IPXC_ContentCreator CC, double cx, double baseLineY);
+		delegate void FillByGradient(IPXC_Document Doc, IPXC_ContentCreator CC, PXC_Rect rect);
 
 		[Description("Add Text with different Text Rendering Mode to the current document")]
 		static public void DrawTextRenderingModesOnPage(Form1 Parent)
 		{
+			const uint argbDarkLime = 0x00008888;
+			const uint argbBlack = 0x00000000;
+			//delegate void DrawTitle(IPXC_Document Doc, IPXC_ContentCreator ContCrt, double cx, double baseLineY, string sText, double fontSize);
+			DrawTitle drawTitle = (Doc, ContCrt, cx, baseLineY, sText, fontSize) =>
+			{
+				IPXC_Font defFont = Doc.CreateNewFont("Arial", 0, 400);
+				ContCrt.SaveState();
+				ContCrt.SetFillColorRGB(argbBlack);
+				ContCrt.SetFont(defFont);
+				double Width = 0;
+				double Height = 0;
+				ContCrt.CalcTextSize(fontSize, sText, out Width, out Height, -1);
+				ContCrt.SetFontSize(fontSize);
+				ContCrt.ShowTextLine(cx - Width / 2.0, baseLineY, sText, -1, (uint)PXC_ShowTextLineFlags.STLF_Default | (uint)PXC_ShowTextLineFlags.STLF_AllowSubstitution);
+				ContCrt.RestoreState();
+			};
+			//delegate IPXC_Pattern CreateImagePattern(string str, IPXC_Document Doc, IIXC_Inst g_ImgCore);
+			CreateImagePattern crtImgPat = (str, doc, ImgCore) =>
+			{
+				IPXC_Pattern Patt = null;
+				IPXC_Image Img = doc.AddImageFromFile(str);
+				PXC_Rect bbox;
+				bbox.left = 0;
+				bbox.bottom = 0;
+				bbox.right = Img.Width * 72.0 / 96.0;
+				bbox.top = Img.Height * 72.0 / 96.0;
+				IPXC_ContentCreator ContCrt = doc.CreateContentCreator();
+
+				PXC_Matrix im = new PXC_Matrix();
+				im.a = bbox.right;
+				im.b = 0;
+				im.c = 0;
+				im.d = bbox.top;
+				im.e = 0;
+				im.f = 0;
+				ContCrt.SaveState();
+				ContCrt.ConcatCS(im);
+				ContCrt.PlaceImage(Img);
+				ContCrt.RestoreState();
+				Patt = doc.CreateTilePattern(ref bbox);
+				IPXC_Content content = ContCrt.Detach();
+				content.set_BBox(ref bbox);
+				Patt.SetContent(content, (uint)PXC_PlaceContentFlags.PlaceContent_Replace);
+				return Patt;
+			};
+			//delegate void FillByGradient(IPXC_Document Doc, IPXC_ContentCreator CC, PXC_Rect rect);
+			FillByGradient fillByGradient = (Doc, ConCret, rect) =>
+			{
+				IPXC_GradientStops Stops;
+				Stops = Doc.CreateShadeStops();
+
+				Stops.AddStopRGB(0.0, 0x00ff0000);
+				Stops.AddStopRGB(1.0, 0x0000ffff);
+
+				IPXC_Shading Shade;
+				PXC_Point p0, p1;
+				p0.x = rect.left; p0.y = rect.top;
+				p1.x = rect.left; p1.y = rect.bottom;
+
+				Shade = Doc.CreateLinearShade(ref p0, ref p1, Stops, 3);
+
+				ConCret.SaveState();
+				ConCret.SetShadeAsPattern(Shade, true);
+				ConCret.SetStrokeColorRGB(argbBlack);
+				ConCret.Rect(rect.left, rect.bottom, rect.right, rect.top);
+				ConCret.FillPath(true, false, PXC_FillRule.FillRule_Winding);
+				ConCret.RestoreState();
+			};
+
 			if (Parent.m_CurDoc == null)
 				Document.CreateNewDoc(Parent);
-#warning Implement this method
+			PXC_Rect rc;
+			rc.left = 0;
+			rc.right = 600;
+			rc.top = 800;
+			rc.bottom = 0;
+
+			IPXC_UndoRedoData urData;
+			IPXC_ContentCreator CC = Parent.m_CurDoc.CreateContentCreator();
+			IPXC_Page Page = Parent.m_CurDoc.Pages.InsertPage(0, ref rc, out urData);
+
+			double x = 1 * 72.0;
+			double y = rc.top - 1 * 72.0;
+			double xs = 3.5 * 72.0;
+			double ys = 1 * 72.0;
+			double ts = 0.45 * 72.0;
+
+			IPXC_Font Font;
+			IPXC_Font Font2;
+			Font = Parent.m_CurDoc.CreateNewFont("Impact", 0, 0);
+			double fntsize = 25.0;
+			string text = "Text Rendering Mode";
+			double nWidth;
+			double nHight;
+
+			CC.SaveState();
+			CC.SetFont(Font);
+			CC.SetFontSize(fntsize);
+			CC.CalcTextSize(fntsize, text, out nWidth, out nHight, -1);
+
+			CC.SetFillColorRGB(argbDarkLime);
+			CC.SaveState();
+			CC.SetTextRenderMode(PXC_TextRenderingMode.TRM_Fill);
+			CC.ShowTextLine(x, y, text, -1, 0);
+			CC.RestoreState();
+			drawTitle(Parent.m_CurDoc, CC, x + nWidth / 2, y - ts, "TRM_Fill", 15);
+
+			x += xs;
+			CC.SaveState();
+			CC.SetTextRenderMode(PXC_TextRenderingMode.TRM_Stroke);
+			CC.ShowTextLine(x, y, text, -1, 0);
+			CC.RestoreState();
+			drawTitle(Parent.m_CurDoc, CC, x + nWidth / 2, y - ts, "TRM_Stroke", 15);
+
+			x -= xs; y -= ys;
+			CC.SaveState();
+			CC.SetTextRenderMode(PXC_TextRenderingMode.TRM_FillStroke);
+			CC.ShowTextLine(x, y, text, -1, 0);
+			CC.RestoreState();
+			drawTitle(Parent.m_CurDoc, CC, x + nWidth / 2, y - ts, "TRM_FillStroke", 15);
+
+			x += xs;
+			CC.SaveState();
+			CC.SetTextRenderMode(PXC_TextRenderingMode.TRM_None);
+			CC.ShowTextLine(x, y, text, -1, 0);
+			CC.RestoreState();
+			drawTitle(Parent.m_CurDoc, CC, x + nWidth / 2, y - ts, "TRM_None", 15);
+
+			x -= xs;
+			y -= ys;
+			double w = 3.2 * 72.0;
+			double h = 1 * 72.0;
+			//
+			text = "ABC";
+			Font2 = Parent.m_CurDoc.CreateNewFont("Arial Black", 0, 0);
+			fntsize = 50;
+			CC.SetFontSize(fntsize);
+			CC.SetFont(Font2);
+
+			PXC_Rect pr;
+			pr.left = x; pr.right = pr.left + w;
+			pr.top = y; pr.bottom = pr.top - h;
+			fillByGradient(Parent.m_CurDoc, CC, pr);
+
+			CC.SaveState();
+			CC.SetCharSpace(2.0);
+			CC.SetTextScale(150.0);
+			CC.SetTextRenderMode(PXC_TextRenderingMode.TRM_Stroke);
+			CC.ShowTextLine(x + 0.35 * 72.0, y, text, -1, 0);
+			CC.RestoreState();
+			drawTitle(Parent.m_CurDoc, CC, x + w / 2, y - 1.1 * 72.0, "TRM_Stroke", 15);
+
+			x += xs;
+			pr.left += xs; pr.right += xs;
+			CC.SaveState();
+			CC.SetCharSpace(2.0);
+			CC.SetTextScale(150.0);
+			CC.SetTextRenderMode(PXC_TextRenderingMode.TRM_Clip_Stroke);
+			CC.ShowTextLine(x + 0.35 * 72.0, y, text, -1, 0);
+			fillByGradient(Parent.m_CurDoc, CC, pr);
+			CC.RestoreState();
+			drawTitle(Parent.m_CurDoc, CC, x + w / 2, y - 1.1 * 72.0, "TRM_Clip_Stroke", 15);
+
+			text = "FILL";
+			CC.SetFont(Font);
+			CC.SetFontSize(120);
+
+
+			x = rc.right / 4; y -= 1.4 * 72.0;
+			pr.left += xs; pr.right += xs;
+			CC.SaveState();
+			CC.SetFillColorRGB(argbDarkLime);
+			CC.SetCharSpace(2.0);
+			CC.SetTextScale(150.0);
+			CC.SetTextRenderMode(PXC_TextRenderingMode.TRM_FillStroke);
+			CC.ShowTextLine(x + 0.35 * 72.0, y, text, -1, 0);
+			CC.RestoreState();
+			drawTitle(Parent.m_CurDoc, CC, rc.right / 2 + 15, y - 1.8 * 72.0, "SOLID COLOR", 15);
+
+			x = rc.right / 4; y -= 1.8 * 72.0;
+			pr.left += xs; pr.right += xs;
+			CC.SaveState();
+			IPXC_Pattern Pat = Parent.m_CurDoc.GetStdTilePattern((PXC_StdPatternType)4);
+			CC.SetPatternRGB(Pat, true, argbDarkLime);
+
+			CC.SetCharSpace(2.0);
+			CC.SetTextScale(150.0);
+			CC.SetTextRenderMode(PXC_TextRenderingMode.TRM_FillStroke);
+			CC.ShowTextLine(x + 0.35 * 72.0, y, text, -1, 0);
+			CC.RestoreState();
+			drawTitle(Parent.m_CurDoc, CC, rc.right / 2 + 15, y - 1.8 * 72.0, "HatchType_Horizontal PATTERN", 15);
+
+
+			x = rc.right / 4; y -= 1.8 * 72.0;
+			pr.left += xs; pr.right += xs;
+			CC.SaveState();
+			IIXC_Inst Ixc_Inst = (IIXC_Inst)Parent.m_pxcInst.GetExtension("IXC");
+			Pat = crtImgPat(System.IO.Directory.GetParent(System.Environment.CurrentDirectory).Parent.FullName + "\\Images\\CoreAPI_32.ico", Parent.m_CurDoc, Ixc_Inst);
+			CC.SetPatternRGB(Pat, true, argbDarkLime);
+			CC.SetCharSpace(2.0);
+			CC.SetTextScale(150.0);
+			CC.SetTextRenderMode(PXC_TextRenderingMode.TRM_FillStroke);
+			CC.ShowTextLine(x + 0.35 * 72.0, y, text, -1, 0);
+			Pat = null;
+			CC.RestoreState();
+			drawTitle(Parent.m_CurDoc, CC, rc.right / 2 + 15, y - 1.8 * 72.0, "IMAGE PATTERN FOR STROKE", 15);
+
+			Page.PlaceContent(CC.Detach(), (uint)PXC_PlaceContentFlags.PlaceContent_Replace);
 		}
 
 		[Description("Add Text with different Char and Word Spacing to the current document")]
 		static public void DrawTextWithDifferentSpacingOnPage(Form1 Parent)
 		{
-			if (Parent.m_CurDoc == null)
-				Document.CreateNewDoc(Parent);
-#warning Implement this method
+			
 		}
 
 		[Description("Add Text with different Scaling, Subscript and Superscript to the current document")]
@@ -366,9 +570,6 @@ namespace CoreAPIDemo
 			CC.StrokePath(false);
 
 			Page.PlaceContent(CC.Detach(), (uint)PXC_PlaceContentFlags.PlaceContent_Replace);
-
-			Parent.UpdateControlsFromDocument();
-			Parent.UpdatePreviewFromCurrentDocument();
 		}
 
 		[Description("Add Fillings with different Styles to the current document")]
@@ -505,69 +706,12 @@ namespace CoreAPIDemo
 				y -= 3.0 * 72.0;
 			}
 			firstPage.PlaceContent(CC.Detach(), (uint)PXC_PlaceContentFlags.PlaceContent_Replace);
-			IIXC_Inst Ixc_Inst = (IIXC_Inst)Parent.m_pxcInst.GetExtension("IXC");
 			IPXC_Page secondPage = Parent.m_CurDoc.Pages.InsertPage(0, ref rc, out urData);
+
 			double w = (rc.right - rc.left - 3 * 72.0) / 2.0;
 			double h = 1 * 72.0;
-			double Y = rc.top - 1 * 72.0 - h;
-			double dy = 2 * 72.0;
-			double[] X = { rc.left + 1.0 * 72.0, rc.left + 5 * 72.0 };
-
-			CC.SetLineWidth(1.0);
-			CC.SetStrokeColorRGB(argbBlack);
-			CC.SetFillColorRGB(argbWhiteGreen);
-
-			CC.Rect(X[0], Y, X[0] + w, Y + h);
-			CC.StrokePath(false);
-			drawTitle(Parent.m_CurDoc, CC, X[0] + w / 2, Y - 0.1 * 72.0, "STROKE, NO FILL", 15);
-
-			CC.Rect(X[1], Y, X[1] + w, Y + h);
-			CC.FillPath(false, false, PXC_FillRule.FillRule_Winding);
-			drawTitle(Parent.m_CurDoc, CC, X[1] + w / 2, Y - 0.1 * 72.0, "FILL, NO STROKE", 15);
-
-			Y -= dy;
-
-			CC.Rect(X[0], Y, X[0] + w, Y + h);
-			CC.FillPath(false, true, PXC_FillRule.FillRule_Winding);
-			drawTitle(Parent.m_CurDoc, CC, X[0] + w / 2, Y - 0.1 * 72.0, "STROKE & FILL", 15);
-			string[] PatternTitles =
-			{
-				"PATTERN FILL: CrossHatch",
-				"PATTERN FILL: CrossDiagonal",
-				"PATTERN FILL: DiagonalLeft",
-				"PATTERN FILL: DiagonalRight",
-				"PATTERN FILL: Horizontal",
-				"PATTERN FILL: Vertical"
-			};
-
-			int k = 1;
-			IPXC_Pattern Pat;
-			for (int i = (int)PXC_StdPatternType.StdPattern_CrossHatch; i <= (int)PXC_StdPatternType.StdPattern_Vertical; i++)
-			{
-				Pat = Parent.m_CurDoc.GetStdTilePattern((PXC_StdPatternType)i);
-				CC.SetPatternRGB(Pat, true, argbWhiteGreen);
-				Pat = null;
-				CC.Rect(X[k], Y, X[k] + w, Y + h);
-				CC.FillPath(false, true, PXC_FillRule.FillRule_Winding);
-				drawTitle(Parent.m_CurDoc, CC, X[k] + w / 2, Y - 0.1 * 72.0, PatternTitles[i], 15);
-				k ^= 1;
-				if (k == 0)
-					Y -= dy;
-			}
-			Pat = crtImgPat(System.IO.Directory.GetParent(System.Environment.CurrentDirectory).Parent.FullName + "\\Images\\CoreAPI_32.ico", Parent.m_CurDoc, Ixc_Inst);
-			CC.SetPatternRGB(Pat, true, argbWhiteGreen);
-			CC.Rect(X[k], Y, X[k] + w, Y + h);
-			CC.FillPath(false, true, PXC_FillRule.FillRule_Winding);
-			drawTitle(Parent.m_CurDoc, CC, X[1] + w / 2, Y - 0.1 * 72.0, "PATTERN FILL: Image", 15);
-
-			secondPage.PlaceContent(CC.Detach(), (uint)PXC_PlaceContentFlags.PlaceContent_Replace);
-
-			IPXC_Page thirdPage = Parent.m_CurDoc.Pages.InsertPage(0, ref rc, out urData);
-
-			w = (rc.right - rc.left - 3 * 72.0) / 2.0;
-			h = 1 * 72.0;
 			y = rc.top - 1.0 * 72.0 - h;
-			dy = 2 * 72.0;
+			double dy = 2 * 72.0;
 			x = rc.left + 1.0 * 72.0;
 
 			IPXC_GradientStops Stops = Parent.m_CurDoc.CreateShadeStops();
@@ -602,7 +746,7 @@ namespace CoreAPIDemo
 			CC.Shade(Shade);
 			drawTitle(Parent.m_CurDoc, CC, x, y - 0.1 * 72.0, "Radial Gradient", 15);
 
-			thirdPage.PlaceContent(CC.Detach(), (uint)PXC_PlaceContentFlags.PlaceContent_Replace);
+			secondPage.PlaceContent(CC.Detach(), (uint)PXC_PlaceContentFlags.PlaceContent_Replace);
 
 			Parent.UpdateControlsFromDocument();
 			Parent.UpdatePreviewFromCurrentDocument();
@@ -612,9 +756,120 @@ namespace CoreAPIDemo
 		[Description("Add Patterns with different Styles to the current document")]
 		static public void DrawPatternsOnPage(Form1 Parent)
 		{
+			const uint argbBlack = 0x00000000;
+			const uint argbDarkLime = 0x00008888;
+			//delegate IPXC_Pattern CreateImagePattern(string str, IPXC_Document Doc, IIXC_Inst g_ImgCore);
+			CreateImagePattern crtImgPat = (str, doc, ImgCore) =>
+			{
+				IPXC_Pattern Patt = null;
+				IPXC_Image Img = doc.AddImageFromFile(str);
+				PXC_Rect bbox;
+				bbox.left = 0;
+				bbox.bottom = 0;
+				bbox.right = Img.Width * 72.0 / 96.0;
+				bbox.top = Img.Height * 72.0 / 96.0;
+				IPXC_ContentCreator ContCrt = doc.CreateContentCreator();
+
+				PXC_Matrix im = new PXC_Matrix();
+				im.a = bbox.right;
+				im.b = 0;
+				im.c = 0;
+				im.d = bbox.top;
+				im.e = 0;
+				im.f = 0;
+				ContCrt.SaveState();
+				ContCrt.ConcatCS(im);
+				ContCrt.PlaceImage(Img);
+				ContCrt.RestoreState();
+				Patt = doc.CreateTilePattern(ref bbox);
+				IPXC_Content content = ContCrt.Detach();
+				content.set_BBox(ref bbox);
+				Patt.SetContent(content, (uint)PXC_PlaceContentFlags.PlaceContent_Replace);
+				return Patt;
+			};
+			//delegate void DrawTitle(IPXC_Document Doc, IPXC_ContentCreator ContCrt, double cx, double baseLineY, string sText, double fontSize);
+			DrawTitle drawTitle = (Doc, ContCrt, cx, baseLineY, sText, fontSize) =>
+			{
+				IPXC_Font defFont = Doc.CreateNewFont("Arial", 0, 400);
+				ContCrt.SaveState();
+				ContCrt.SetFillColorRGB(argbBlack);
+				ContCrt.SetFont(defFont);
+				double nWidth = 0;
+				double nHeight = 0;
+				ContCrt.CalcTextSize(fontSize, sText, out nWidth, out nHeight, -1);
+				ContCrt.SetFontSize(fontSize);
+				ContCrt.ShowTextLine(cx - nWidth / 2.0, baseLineY, sText, -1, (uint)PXC_ShowTextLineFlags.STLF_Default | (uint)PXC_ShowTextLineFlags.STLF_AllowSubstitution);
+				ContCrt.RestoreState();
+			};
+
 			if (Parent.m_CurDoc == null)
 				Document.CreateNewDoc(Parent);
-#warning Implement this method
+
+			PXC_Rect rc;
+			rc.left = 0;
+			rc.right = 600;
+			rc.top = 800;
+			rc.bottom = 0;
+
+			IPXC_UndoRedoData urData;
+			IPXC_ContentCreator CC = Parent.m_CurDoc.CreateContentCreator();
+			IIXC_Inst Ixc_Inst = (IIXC_Inst)Parent.m_pxcInst.GetExtension("IXC");
+			IPXC_Page Page = Parent.m_CurDoc.Pages.InsertPage(0, ref rc, out urData);
+
+			double w = (rc.right - rc.left - 3 * 72.0) / 2.0;
+			double h = 1 * 72.0;
+			double Y = rc.top - 1 * 72.0 - h;
+			double dy = 2 * 72.0;
+			double[] X = { rc.left + 1.0 * 72.0, rc.left + 5 * 72.0 };
+
+			CC.SetLineWidth(1.0);
+			CC.SetStrokeColorRGB(argbBlack);
+			CC.SetFillColorRGB(argbDarkLime);
+
+			CC.Rect(X[0], Y, X[0] + w, Y + h);
+			CC.StrokePath(false);
+			drawTitle(Parent.m_CurDoc, CC, X[0] + w / 2, Y - 0.1 * 72.0, "STROKE, NO FILL", 15);
+
+			CC.Rect(X[1], Y, X[1] + w, Y + h);
+			CC.FillPath(false, false, PXC_FillRule.FillRule_Winding);
+			drawTitle(Parent.m_CurDoc, CC, X[1] + w / 2, Y - 0.1 * 72.0, "FILL, NO STROKE", 15);
+
+			Y -= dy;
+
+			CC.Rect(X[0], Y, X[0] + w, Y + h);
+			CC.FillPath(false, true, PXC_FillRule.FillRule_Winding);
+			drawTitle(Parent.m_CurDoc, CC, X[0] + w / 2, Y - 0.1 * 72.0, "STROKE & FILL", 15);
+			string[] PatternTitles =
+			{
+				"PATTERN FILL: CrossHatch",
+				"PATTERN FILL: CrossDiagonal",
+				"PATTERN FILL: DiagonalLeft",
+				"PATTERN FILL: DiagonalRight",
+				"PATTERN FILL: Horizontal",
+				"PATTERN FILL: Vertical"
+			};
+
+			int k = 1;
+			IPXC_Pattern Pat;
+			for (int i = (int)PXC_StdPatternType.StdPattern_CrossHatch; i <= (int)PXC_StdPatternType.StdPattern_Vertical; i++)
+			{
+				Pat = Parent.m_CurDoc.GetStdTilePattern((PXC_StdPatternType)i);
+				CC.SetPatternRGB(Pat, true, argbDarkLime);
+				Pat = null;
+				CC.Rect(X[k], Y, X[k] + w, Y + h);
+				CC.FillPath(false, true, PXC_FillRule.FillRule_Winding);
+				drawTitle(Parent.m_CurDoc, CC, X[k] + w / 2, Y - 0.1 * 72.0, PatternTitles[i], 15);
+				k ^= 1;
+				if (k == 0)
+					Y -= dy;
+			}
+			Pat = crtImgPat(System.IO.Directory.GetParent(System.Environment.CurrentDirectory).Parent.FullName + "\\Images\\CoreAPI_32.ico", Parent.m_CurDoc, Ixc_Inst);
+			CC.SetPatternRGB(Pat, true, argbDarkLime);
+			CC.Rect(X[k], Y, X[k] + w, Y + h);
+			CC.FillPath(false, true, PXC_FillRule.FillRule_Winding);
+			drawTitle(Parent.m_CurDoc, CC, X[1] + w / 2, Y - 0.1 * 72.0, "PATTERN FILL: Image", 15);
+
+			Page.PlaceContent(CC.Detach(), (uint)PXC_PlaceContentFlags.PlaceContent_Replace);
 		}
 
 		[Description("Add content with different Coordinate System Transformations (matrix usages)")]
@@ -784,6 +1039,7 @@ namespace CoreAPIDemo
 
 			Parent.UpdateControlsFromDocument();
 			Parent.UpdatePreviewFromCurrentDocument();*/
+#warning Implement this method
 		}
 	}
 }
