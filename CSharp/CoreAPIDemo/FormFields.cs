18a08ddb652fd6234048773ef888050487f98dc2
Arsen Senkiv <senkivarsen@gmail.com>
2018-07-04 15:41:31 +0300

    Add sample about text field.

--- CSharp/CoreAPIDemo/FormFields.cs
+++ CSharp/CoreAPIDemo/FormFields.cs
@@ -9,13 +9,68 @@ namespace CoreAPIDemo
 		[Description("5.1. Add Text Fields on page")]
 		static public void AddTextFieldsOnPage(Form1 Parent)
 		{
+			if (Parent.m_CurDoc == null)
+				Document.CreateNewDoc(Parent);
+			PXC_Rect rc = new PXC_Rect();
+			rc.top = 800;
+			rc.right = 600;
+
+			IPXC_UndoRedoData urD = null;
+			IPXC_Page firstPage = Parent.m_CurDoc.Pages.InsertPage(0, rc, out urD);
+			PXC_Rect textRC = new PXC_Rect();
+			textRC.top = rc.top - 1.0 * 72.0;
+			textRC.left = 1.0 * 72.0;
+			textRC.bottom = rc.top - 2.0 * 72.0;
+			textRC.right = rc.right - 1.0 * 72.0;
+
 			//Ordinary text field
+			IPXC_FormField firstTextBOX = Parent.m_CurDoc.AcroForm.CreateField("Text1", PXC_FormFieldType.FFT_Text, 0, textRC);
+			firstTextBOX.SetValueText("Ordinary text field");
 
 			//Read-only and locked text field with custom style
+			textRC.top = rc.top - 3.0 * 72.0;
+			textRC.bottom = rc.top - 4.0 * 72.0;
+			IPXC_FormField secondTextBOX = Parent.m_CurDoc.AcroForm.CreateField("Text2", PXC_FormFieldType.FFT_Text, 0, textRC);
+			secondTextBOX.SetValueText("Read-only and locked text field with custom style");
+			IAUX_Inst auxInst = Parent.m_pxcInst.GetExtension("AUX");
+			IPXC_Annotation annot = secondTextBOX.Widget[0];
+			IPXC_AnnotData_Widget WData = (IPXC_AnnotData_Widget)annot.Data;
+			IColor color = auxInst.CreateColor(ColorType.ColorType_RGB);
+			color.SetRGB(0.9f, 0.9f, 0.6f);
+			WData.FColor = color;
+			color.SetRGB(0.6f, 0.9f, 0.9f);
+			WData.SColor = color;
+			PXC_AnnotBorder border = new PXC_AnnotBorder();
+			border.nStyle = PXC_AnnotBorderStyle.ABS_Dashed;
+			border.DashArray = new float[10];
+			border.DashArray[0] = border.DashArray[1] = 16.0f; //Width of dashes
+			border.nDashCount = 2; //Number of dashes
+			border.nWidth = 5.0f;
+			WData.set_Border(ref border);
+			annot.Data = WData;
+			annot.Flags = (uint)PXC_AnnotFlag.AF_Locked;
+			secondTextBOX.SetFlags((uint)PXC_FormFieldFlag.FFF_ReadOnly, 1);
 
 			//90 degree orientation text field with multiline option enabled
-
+			textRC.top = rc.top - 5.0 * 72.0;
+			textRC.bottom = rc.top - 7.0 * 72.0;
+			IPXC_FormField thirdTextBOX = Parent.m_CurDoc.AcroForm.CreateField("Text3", PXC_FormFieldType.FFT_Text, 0, textRC);
+			thirdTextBOX.SetFlags((uint)PXC_FormFieldFlag.TFF_MultiLine, 4096);
+			thirdTextBOX.SetValueText("90 degree orientation text field with multiline option enabled");
+			annot = thirdTextBOX.Widget[0];
+			WData = (IPXC_AnnotData_Widget)annot.Data;
+			WData.ContentRotation = 90;
+			annot.Data = WData;
+			
 			//Time formatted text field with custom JS that gives current time
+			textRC.top = rc.top - 8.0 * 72.0;
+			textRC.bottom = rc.top - 9.0 * 72.0;
+			IPXC_FormField fourthTextBOX = Parent.m_CurDoc.AcroForm.CreateField("Text4", PXC_FormFieldType.FFT_Text, 0, textRC);
+			annot = fourthTextBOX.Widget[0];
+			IPXC_ActionsList actionsList = Parent.m_CurDoc.CreateActionsList();
+			actionsList.AddJavaScript("var textBOX = this.getField(\"Text4\");" + 
+				"textBOX.value = new Date();");
+			Parent.m_CurDoc.Pages[0].Actions[PXC_TriggerType.Trigger_PageOpened] = actionsList;
 		}
 
 		[Description("5.2. Add Button form field with icon and an URI Action link")]
