0c8fd67f1083031a21772bb2eceba2924be20cda
Arsen Senkiv <senkivarsen@gmail.com>
2018-07-10 17:03:53 +0300

    Add sample about convert from pdf to png without background, to jpeg as gray, to tiff as 1 BPP, but tiff don`t work :(

---
+++ CSharp/CoreAPIDemo/Converters.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.ComponentModel;
+using PDFXCoreAPI;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace CoreAPIDemo
+{
+	[Description("8. Converters")]
+	class Converters
+	{
+		[Description("8.1. Convert from PDF to image")]
+		static public void ConvertToImage(Form1 Parent)
+		{
+			if (Parent.m_CurDoc == null)
+				Document.CreateNewDoc(Parent);
+
+			IIXC_Inst ixcInst = Parent.m_pxcInst.GetExtension("IXC");
+			IAUX_Inst auxInst = Parent.m_pxcInst.GetExtension("AUX");
+			IPXC_Page Page = Parent.m_CurDoc.Pages[Parent.CurrentPage];
+			double nHeight = 0.0;
+			double nWidth = 0.0;
+			Page.GetDimension(out nWidth, out nHeight);
+			uint cx = (uint)(nWidth * 300 / 72.0);
+			uint cy = (uint)(nHeight * 300 / 72.0);
+			IIXC_Page ixcPage = ixcInst.Page_CreateEmpty(cx, cy, IXC_PageFormat.PageFormat_8ARGB, 0);
+			IPXC_PageRenderParams param = Parent.m_pxcInst.CreateRenderParams();
+			if (param != null)
+			{
+				param.RenderFlags |= ((uint)PXC_RenderFlags.RF_SmoothImages | (uint)PXC_RenderFlags.RF_SmoothLineArts);
+				param.SetColor(PXC_RenderColor.RC_PageColor1, 0, 0, 0, 0);
+				param.TextSmoothMode |= PXC_TextSmoothMode.TSM_Antialias;
+			}
+			tagRECT rc = new tagRECT();
+			rc.right = (int)cx;
+			rc.bottom = (int)cy;
+			PXC_Matrix matrix = Page.GetMatrix(PXC_BoxType.PBox_PageBox);
+			matrix = auxInst.MathHelper.Matrix_Scale(ref matrix, cx / nWidth, -cy / nHeight);
+			matrix = auxInst.MathHelper.Matrix_Translate(ref matrix, 0, cy);
+			Page.DrawToIXCPage(ixcPage, ref rc, ref matrix, param);
+			ixcPage.FmtInt[(uint)IXC_FormatParametersIDS.FP_ID_XDPI] = 300;
+			ixcPage.FmtInt[(uint)IXC_FormatParametersIDS.FP_ID_YDPI] = 300;
+			ixcPage.FmtInt[(uint)IXC_FormatParametersIDS.FP_ID_FORMAT] = (uint)IXC_ImageFileFormatIDs.FMT_PNG_ID;
+			IIXC_Image ixcImg = ixcInst.CreateEmptyImage();
+			ixcImg.InsertPage(ixcPage, 0);
+			string sPath = Path.GetTempFileName();
+			sPath = sPath.Replace(".tmp", ".png");
+			ixcImg.Save(sPath, IXC_CreationDisposition.CreationDisposition_Overwrite);
+			Process pr = Process.Start(sPath);
+
+			ixcImg.RemovePageByIndex(0);
+			ixcPage = ixcInst.Page_CreateEmpty(cx, cy, IXC_PageFormat.PageFormat_8Gray, 0);
+			param = Parent.m_pxcInst.CreateRenderParams();
+			if (param != null)
+			{
+				param.RenderFlags |= ((uint)PXC_RenderFlags.RF_OverrideBackgroundColor | (uint)PXC_RenderFlags.RF_SmoothImages | (uint)PXC_RenderFlags.RF_SmoothLineArts);
+				param.TextSmoothMode |= PXC_TextSmoothMode.TSM_Antialias;
+			}
+			Page.DrawToIXCPage(ixcPage, ref rc, ref matrix, param);
+			ixcPage.FmtInt[(uint)IXC_FormatParametersIDS.FP_ID_XDPI] = 300;
+			ixcPage.FmtInt[(uint)IXC_FormatParametersIDS.FP_ID_YDPI] = 300;
+			ixcPage.FmtInt[(uint)IXC_FormatParametersIDS.FP_ID_FORMAT] = (uint)IXC_ImageFileFormatIDs.FMT_JPEG_ID;
+			ixcPage.FmtInt[(uint)IXC_FormatParametersIDS.FP_ID_JPEG_QUALITY] = 100;
+			ixcImg.InsertPage(ixcPage, 0);
+			sPath = sPath.Replace(".png", ".jpg");
+			ixcImg.Save(sPath, IXC_CreationDisposition.CreationDisposition_Overwrite);
+			pr = Process.Start(sPath);
+
+			ixcImg.RemovePageByIndex(0);
+			ixcPage = ixcInst.Page_CreateEmpty(cx, cy, IXC_PageFormat.PageFormat_8RGB, 0);
+			param = Parent.m_pxcInst.CreateRenderParams();
+			if (param != null)
+			{
+				param.RenderFlags |= ((uint)PXC_RenderFlags.RF_OverrideBackgroundColor | (uint)PXC_RenderFlags.RF_SmoothImages | (uint)PXC_RenderFlags.RF_SmoothLineArts);
+				param.TextSmoothMode |= PXC_TextSmoothMode.TSM_Antialias;
+			}
+			Page.DrawToIXCPage(ixcPage, ref rc, ref matrix, param);
+			ixcPage.FmtInt[(uint)IXC_FormatParametersIDS.FP_ID_XDPI] = 300;
+			ixcPage.FmtInt[(uint)IXC_FormatParametersIDS.FP_ID_YDPI] = 300;
+			ixcPage.FmtInt[(uint)IXC_FormatParametersIDS.FP_ID_BPC] = 1;
+			ixcPage.FmtInt[(uint)IXC_FormatParametersIDS.FP_ID_BPP] = 1;
+			ixcPage.FmtInt[(uint)IXC_FormatParametersIDS.FP_ID_DITHER] = 1;
+			ixcPage.FmtInt[(uint)IXC_FormatFlags.FMTF_CAN_Encode] = 1;
+			ixcPage.FmtInt[(uint)IXC_FormatParametersIDS.FP_ID_FORMAT] = (uint)IXC_ImageFileFormatIDs.FMT_TIFF_ID;
+			ixcImg.InsertPage(ixcPage, 0);
+			sPath = sPath.Replace(".jpg", ".tiff");
+			ixcImg.Save(sPath, IXC_CreationDisposition.CreationDisposition_Overwrite);
+			pr = Process.Start(sPath);
+
+#warning Not implemented
+		}
+
+		[Description("8.2. Convert from image to PDF")]
+		static public void ConvertToPDF(Form1 Parent)
+		{
+			if (Parent.m_CurDoc == null)
+				Document.CreateNewDoc(Parent);
+#warning Not implemented
+		}
+
+		[Description("8.3. Convert from PDF to txt file")]
+		static public void ConvertToTXT(Form1 Parent)
+		{
+			if (Parent.m_CurDoc == null)
+				Document.CreateNewDoc(Parent);
+#warning Not implemented
+		}
+	}
+}
--- CSharp/CoreAPIDemo/Pagemarks.cs
+++ CSharp/CoreAPIDemo/Pagemarks.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel;
+using System;
 using PDFXCoreAPI;
+
 namespace CoreAPIDemo
 {
 	[Description("7. Pagemarks")]
@@ -8,29 +10,83 @@ namespace CoreAPIDemo
 		[Description("7.1. Add Headers and Footers on pages")]
 		static public void AddHeadersAndFootersOnPages(Form1 Parent)
 		{
+			if (Parent.m_CurDoc == null)
+				Document.CreateNewDoc(Parent);
+
 			IAUX_Inst auxInst = Parent.m_pxcInst.GetExtension("AUX");
 			IPXC_HeaderAndFooterParams HeaderFooter = Parent.m_pxcInst.CreateHeaderAndFooterParams();
+			IPXC_Font font = Parent.m_CurDoc.CreateNewFont("Arial", 0, 400);
+			HeaderFooter.Font = font;
 			HeaderFooter.RightFooterText = "%[Page]";
-			IBitSet bitSet = auxInst.CreateBitSet(8192);
+			HeaderFooter.BottomMargin = 36.0f;
+			HeaderFooter.TopMargin = 36.0f;
+			HeaderFooter.RightMargin = 36.0f;
+			HeaderFooter.LeftMargin = 36.0f;
+			IBitSet bitSet = auxInst.CreateBitSet(8);
+			bitSet.SetSize(Parent.m_CurDoc.Pages.Count);
+			bitSet.Set(0, Parent.m_CurDoc.Pages.Count, true);
 			Parent.m_CurDoc.PlaceHeadersAndFooters(bitSet, HeaderFooter);
 		}
 
 		[Description("7.2. Add Watermarks on page")]
 		static public void AddWatermarksOnPage(Form1 Parent)
 		{
-#warning Add one text watermark with custom styling and one image watermark on page
+			if (Parent.m_CurDoc == null)
+				Document.CreateNewDoc(Parent);
+
+			IAUX_Inst auxInst = Parent.m_pxcInst.GetExtension("AUX");
+			IPXC_WatermarkParams watermark = Parent.m_pxcInst.CreateWatermarkParams();
+			IColor fillColor = auxInst.CreateColor(ColorType.ColorType_Gray);
+			fillColor.SetRGB(0.7f, 0.7f, 0.7f);
+			watermark.Text = "WATERMARK";
+			watermark.HAlign = 50;
+			watermark.VAlign = 50;
+			watermark.FillColor = fillColor;
+			watermark.Rotation = -45;
+			watermark.FontSize = 200;
+			watermark.StrokeWidth = 5.0f;
+			IBitSet bitSet = auxInst.CreateBitSet(8);
+			bitSet.SetSize(Parent.m_CurDoc.Pages.Count);
+			bitSet.Set(0, Parent.m_CurDoc.Pages.Count, true);
+			Parent.m_CurDoc.PlaceWatermark(bitSet, watermark);
 		}
 
 		[Description("7.3. Add Background on page")]
 		static public void AddBackgroundOnPage(Form1 Parent)
 		{
-#warning Add one background on the top of the page and one from file on the bottom of the page with different stylings
+			if (Parent.m_CurDoc == null)
+				Document.CreateNewDoc(Parent);
+
+			IAUX_Inst auxInst = Parent.m_pxcInst.GetExtension("AUX");
+			IPXC_BackgroundParams backgroundParams = Parent.m_pxcInst.CreateBackgroundParams();
+			IColor fillColor = auxInst.CreateColor(ColorType.ColorType_Gray);
+			fillColor.SetRGB(1.0f, 0.0f, 0.0f);
+			IBitSet bitSet = auxInst.CreateBitSet(8);
+			bitSet.SetSize(Parent.m_CurDoc.Pages.Count);
+			bitSet.Set(0, Parent.m_CurDoc.Pages.Count, true);
+			backgroundParams.FillColor = fillColor;
+
+			Parent.m_CurDoc.PlaceBackgrounds(bitSet, backgroundParams);
 		}
 
 		[Description("7.4. Remove Headers and Footers from page")]
 		static public void RemoveHeadersAndFooters(Form1 Parent)
 		{
-#warning Remove all of the Headers and Footers on page
+			//if (Parent.m_CurDoc == null)
+			//	return;
+			//
+			////Get current page
+			//if (Parent.CurrentPage >= Parent.m_CurDoc.Pages.Count)
+			//	return;
+			//
+			//IPXC_Page Page = Parent.m_CurDoc.Pages[Parent.CurrentPage];
+			//IPXC_DocumentProps docProps = Parent.m_CurDoc.Props;
+			////docProps
+			//Parent.m_CurDoc.
+			////Remove all fields from page
+			//IPXC_Content content = Page.GetContent(PXC_ContentAccessMode.CAccessMode_FullClone);
+			////content.
+#warning Remove all of the header and footer from page
 		}
 
 		[Description("7.5. Remove Watermarks from page")]
