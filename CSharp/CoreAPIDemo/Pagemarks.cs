0707a749bc642c05ccfa947ecfc83ca09036df36
Arsen Senkiv <senkivarsen@gmail.com>
2018-07-14 14:04:03 +0300

    Corrected sample about watermarks

--- CSharp/CoreAPIDemo/Pagemarks.cs
+++ CSharp/CoreAPIDemo/Pagemarks.cs
@@ -18,14 +18,19 @@ namespace CoreAPIDemo
 			IPXC_Font font = Parent.m_CurDoc.CreateNewFont("Arial", 0, 400);
 			IColor color = auxInst.CreateColor(ColorType.ColorType_Gray);
 			color.SetRGB(0, 0, 0);
-			HeaderFooter.FillColor = color;
 			HeaderFooter.Font = font;
+			HeaderFooter.FillColor = color;
 			HeaderFooter.RightFooterText = "%[Page]";
+			HeaderFooter.LeftFooterText = "%[Page] from %[Pages]";
+			HeaderFooter.CenterFooterText = "%[Date:MM.dd.yyyy] %[Time]";
+			HeaderFooter.LeftHeaderText = "Version %[AppVersion]";
+			HeaderFooter.CenterHeaderText = "%[Computer]";
+			HeaderFooter.RightHeaderText = "%[User]";
 			HeaderFooter.BottomMargin = 36.0f;
 			HeaderFooter.TopMargin = 36.0f;
 			HeaderFooter.RightMargin = 36.0f;
 			HeaderFooter.LeftMargin = 36.0f;
-			HeaderFooter.FontSize = 14.0f;
+			HeaderFooter.FontSize = 10.0f;
 			IBitSet bitSet = auxInst.CreateBitSet(8);
 			bitSet.SetSize(Parent.m_CurDoc.Pages.Count);
 			bitSet.Set(0, Parent.m_CurDoc.Pages.Count, true);
@@ -44,14 +49,26 @@ namespace CoreAPIDemo
 			fillColor.SetRGB(0.7f, 0.7f, 0.7f);
 			watermark.Text = "WATERMARK";
 			watermark.HAlign = 50;
-			watermark.VAlign = 50;
+			watermark.VAlign = 0;
 			watermark.FillColor = fillColor;
-			watermark.Rotation = -45;
+			watermark.Rotation = -30;
 			watermark.FontSize = 200;
 			watermark.StrokeWidth = 5.0f;
 			IBitSet bitSet = auxInst.CreateBitSet(8);
 			bitSet.SetSize(Parent.m_CurDoc.Pages.Count);
 			bitSet.Set(0, Parent.m_CurDoc.Pages.Count, true);
+			Parent.m_CurDoc.PlaceWatermark(bitSet, watermark);
+			watermark.Text = "";
+			watermark.ImageFile = System.Environment.CurrentDirectory + "\\Images\\Editor_welcome.png";
+			PXC_Size rect = new PXC_Size();
+			rect.cx = 200;
+			rect.cy = 400;
+			watermark.Rotation = -45;
+			watermark.VAlign = 2;
+			watermark.set_ImageSize(rect);
+			watermark.Flags |= (uint)PXC_WatermarkFlags.WatermarkFlag_PlaceOnBackground;
+			watermark.WatermarkType = PXC_WatermarkType.Watermark_Image;
+			watermark.Opacity = 50;
 			Parent.m_CurDoc.PlaceWatermark(bitSet, watermark);
 		}
 
@@ -64,13 +81,19 @@ namespace CoreAPIDemo
 			IAUX_Inst auxInst = Parent.m_pxcInst.GetExtension("AUX");
 			IPXC_BackgroundParams backgroundParams = Parent.m_pxcInst.CreateBackgroundParams();
 			IColor fillColor = auxInst.CreateColor(ColorType.ColorType_Gray);
-			fillColor.SetRGB(1.0f, 0.0f, 0.0f);
-			IBitSet bitSet = auxInst.CreateBitSet(8);
-			bitSet.SetSize(Parent.m_CurDoc.Pages.Count);
-			bitSet.Set(0, Parent.m_CurDoc.Pages.Count, true);
+			fillColor.SetRGB(0.5f, 1.0f, 0.5f);
+			IBitSet firstBitSet = auxInst.CreateBitSet(8);
+			firstBitSet.SetSize(Parent.m_CurDoc.Pages.Count);
+			firstBitSet.Set(0, Parent.m_CurDoc.Pages.Count - Parent.m_CurDoc.Pages.Count / 2, true);
 			backgroundParams.FillColor = fillColor;
-
-			Parent.m_CurDoc.PlaceBackgrounds(bitSet, backgroundParams);
+			Parent.m_CurDoc.PlaceBackgrounds(firstBitSet, backgroundParams);
+			backgroundParams.BackgroundFile = System.Environment.CurrentDirectory + "\\Images\\Editor_welcome.png";
+			IBitSet secondBitSet = auxInst.CreateBitSet(8);
+			secondBitSet.SetSize(Parent.m_CurDoc.Pages.Count);
+			secondBitSet.Set(Parent.m_CurDoc.Pages.Count / 2, Parent.m_CurDoc.Pages.Count - 1, true);
+			backgroundParams.BackgroundType = PXC_BackgroundType.Background_Image;
+			backgroundParams.Flags |= (uint)PXC_BackgroundFlags.BackgroundFlag_ScaleToPage;
+			Parent.m_CurDoc.PlaceBackgrounds(secondBitSet, backgroundParams);
 		}
 
 		[Description("7.4. Remove Headers and Footers from page")]
