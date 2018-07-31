95920763f47f4765975fec6180269f6efed976fe
Arsen Senkiv <senkivarsen@gmail.com>
2018-07-31 15:26:29 +0300

    Add file Annotations.cs

---
+++ CSharp/CoreAPIDemo/Annotations.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using PDFXCoreAPI;
+
+namespace CoreAPIDemo
+{
+	class Annotations
+	{
+		[Description("7. Annotations")]
+		class NamedDestinations
+		{
+			[Description("7.1. Text annotation")]
+			static public void AddTextAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.2. Link annotation")]
+			static public void AddLinkAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.3. Free text annotation")]
+			static public void AddFreeTextAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.4. Line annotation")]
+			static public void AddLineAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.5. Square annotation")]
+			static public void AddSquareAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.6. Circle annotation")]
+			static public void AddCircleAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.7. Polygon annotation")]
+			static public void AddPolygonAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.8. Polyline annotation")]
+			static public void AddPolylineAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.9. Highlight annotation")]
+			static public void AddHighlightAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.10. Underline annotation")]
+			static public void AddUnderlineAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.11. Squiggly-underline annotation")]
+			static public void AddSquiggly_UnderlineAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.12. Strikeout annotation")]
+			static public void AddStrikeoutAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.13. Caret annotation")]
+			static public void AddCaretAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.14. Ink annotation")]
+			static public void AddInkAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.15. Popup annotation")]
+			static public void AddPopupAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.16. File attachment annotation")]
+			static public void AddFile_AttachmentAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.17. Sound annotation")]
+			static public void AddSoundAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.18. Screen annotation")]
+			static public void AddScreenAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.19. Printer’s mark annotation")]
+			static public void AddPrintersMarkAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.20. Trap network annotation")]
+			static public void AddTrapNetworkAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.21. 3D annotation")]
+			static public void Add3DAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.22. Redact annotation")]
+			static public void AddRedactAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.23. Projection annotation")]
+			static public void AddProjectionAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+
+			[Description("7.24. RichMedia annotation")]
+			static public void AddRichMediaAnnotation(Form1 Parent)
+			{
+#warning Implement this
+			}
+		}
+	}
+}
